588341dc861b9d9503c8b3c74ce598ea7972db70
Ankur <34418414+Ankuraxz@users.noreply.github.com>
2020-07-29 21:37:49 +0530

    Red_dot V2

--- Red_dot.py
+++ Red_dot.py
@@ -1,5 +1,26 @@
 import pyrebase
+import os
+import sys
 import time
+from cv2 import cv2 as cv
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+import requests
+import json
+from twilio.rest import Client #REST API
+import datetime
+import smtplib 
+import imghdr 
+from email.message import EmailMessage 
+import pyrebase
+import collections
+import requests
+from pyowm import OWM
+import sqlite3
+
+
 config = {
     "apiKey": "AIzaSyDienGaKvTKGuSgKIFsU1pupYC1DABpeYk",
     "authDomain": "aghanya-test-py.firebaseapp.com",
@@ -35,14 +56,27 @@ def distance(lon1,lat1,lon2,lat2):
     # calculate the result 
     return(c * r) 
     
+def sms(to,loc,link):
 
+    acc_sid = "AC53b30fe714a03aa740462bd9cb36fcf4"
+    auth_token = "4c217ba652c3ead79b7bda0d3ba96c4c"
+
+    client = Client(acc_sid,auth_token)
+    # for _ in range (len(tag)):
+    #     tag1 = str(tag[_])
+    #     prob1 = str(prob[_])
+    msg = "Animal is detected at "+ loc +" Location link:" +link
+    message = client.messages.create(
+            body = msg,
+            to = to,
+            from_ ='+13343674489') # from is predefined
 
 
 #STREAMING in "ANDROID" child
 def stream_handler(message):
-    print(message["event"]) # put
-    print(message["path"]) # /-K7yGTTEp7O549EzTYtI
-    print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
+    # print(message["event"]) # put
+    # print(message["path"]) # /-K7yGTTEp7O549EzTYtI
+    # print(message["data"]) # {'title': 'Pyrebase', "body": "etc..."}
     # print(type(message["data"]))
     
     if type(message["data"])== dict:
@@ -61,7 +95,7 @@ def stream_handler(message):
         lon2 =float(loc[i+1:])
         # print(lon)
         
-        d=[]
+        d=[] #distance betweeb node and loc.
         total_nodes = 6 
         nodes = {"red1" :[29.965470, 76.891908],
                     "red2" : [29.970303, 76.875890],
@@ -69,14 +103,44 @@ def stream_handler(message):
                     "red4" : [29.954427, 76.851762],
                     "red5" : [29.956509, 76.866385],
                     "red6" : [29.955431, 76.881695]}
+        red = list(nodes.keys())
         for ix in nodes.values():
             lat1 = ix[0]
             lon1 = ix[1]
             dist = distance(lon1,lat1,lon2,lat2)
             d.append(dist)
 
-        print(min(d))
-        print(d.index(min(d)))
+        m = min(d)
+        num = d.index(m)
+        # print(num)
+        # Numbers
+        n = red[num]
+        # https://www.google.com/maps/place/29.9717158,76.8836256
+        link = "https://www.google.com/maps/place/"+str(lat2)+","+str(lon1)
+        loc = str(lat2)+","+str(lon1)
+        print("NOTIFICATION SENT TO POC: " + n)
+        # print(link)
+        #MESSAGING
+        if num ==0 or num ==1:
+            to = '+919996492589'
+            print("MSG SENT TO ANKUR")
+            sms(to,loc,link)
+        elif num ==2 or num ==3:
+            to = '+917404533915'
+            print("MSG SENT TO RISHAB ")
+            sms(to,loc,link)
+        elif num == 4:
+            to='+918628809295'
+            print("MSG SENT TO AMAN ")
+            sms(to,loc,link)
+        else:
+            to='+919996195883'
+            print("MSG SENT TO AANCHAL ")
+            sms(to,loc,link)
+        
+
+
+        
 
 
 my_stream = db.child("Android").stream(stream_handler)
